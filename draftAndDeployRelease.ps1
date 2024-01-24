bde96c4c0e8add1c5e6edf1723f6f7854ed209fb
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2024-01-24 21:08:44 +0100

    Create draftAndDeployRelease.ps1

---
+++ draftAndDeployRelease.ps1
@@ -0,0 +1,45 @@
+param (
+  [Parameter(Mandatory)]
+  $UserName,
+  
+  [Parameter(Mandatory)]
+  $Repository,
+
+  [Parameter(Mandatory)]
+  $ReleaseNotes,
+
+  [Parameter(Mandatory)]
+  $ReleaseName,
+
+  [Paramameter(Mandatory)]
+  $Tag,
+
+  [Parameter(Mandatory)]
+  $GHToken
+)
+
+try {
+		$splatParams = @{
+			Uri         = "https://api.github.com/repos/$UserName/$Repository/releases"
+			Method      = 'POST'
+			Headers     = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($GHToken + ":x-oauth-basic"))}
+			ContentType = 'application/json'
+			Body        = @{
+        tag_name         = $Tag
+        target_commitish = 'main'
+        name             = $ReleaseName
+        body             = $ReleaseNotes
+        draft            = $false
+        prerelease       = $false
+      } | ConvertTo-Json
+		}
+  $response = Invoke-RestMethod @splatParams
+  # Check output of response
+} catch {
+  $PSCmdlet.ThrowTerminatingError($_)
+}
+
+
+# Write back the generated release to workflow
+echo "::set-output name=tag::$($response.tag_name)"
+echo "::set-output name=notes::$($response.description)"
