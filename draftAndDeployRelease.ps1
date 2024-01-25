38f4f410c2a15cd1de0b70b8496fe553cd72c83d
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2024-01-25 20:31:31 +0100

    updated

--- draftAndDeployRelease.ps1
+++ draftAndDeployRelease.ps1
@@ -6,38 +6,31 @@ param (
   $Repository,
 
   [Parameter(Mandatory)]
-  $ReleaseNotes,
-
-  [Parameter(Mandatory)]
-  $ReleaseName,
-
-  [Parameter(Mandatory)]
-  $Tag,
-
-  [Parameter(Mandatory)]
   $GHToken
 )
 
 try {
-		$splatParams = @{
-			Uri         = "https://api.github.com/repos/$UserName/$Repository/releases"
-			Method      = 'POST'
-			Headers     = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($GHToken + ":x-oauth-basic"))}
-			ContentType = 'application/json'
-			Body        = @{
-        tag_name         = $Tag
-        target_commitish = 'main'
-        name             = $ReleaseName
-        body             = $ReleaseNotes
-        draft            = $false
-        prerelease       = $false
+  $changelog = Get-Content -Path 'CHANGELOG.md' -Raw
+  $versionTag = $changelog -match '## \[([^\]]+)' | Select-Object -First 1 -ExpandProperty Matches | ForEach-Object { $_.Groups[1].Value }
+
+  $splatParams = @{
+      Uri         = "https://api.github.com/repos/$UserName/$Repository/releases"
+      Method      = 'POST'
+      Headers     = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($GHToken + ":x-oauth-basic")) }
+      ContentType = 'application/json'
+      Body        = @{
+          tag_name         = $versionTag
+          target_commitish = 'main'
+          name             = 'Initial'
+          body             = 'Initial release'
+          draft            = $false
+          prerelease       = $false
       } | ConvertTo-Json
-		}
+  }
   $response = Invoke-RestMethod @splatParams
+  $message = "Created release with tag: $($response.tag_name)"
 } catch {
   $PSCmdlet.ThrowTerminatingError($_)
 }
 
-# Write back the generated release to workflow
-echo "::set-output name=tag::$($response.tag_name)"
-echo "::set-output name=notes::$($response.description)"
+echo "::set-output name=message::$message"
