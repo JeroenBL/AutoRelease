eb2326ce9859636720a0f2109ff1fe82b71c104f
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2024-01-25 20:59:22 +0100

    updated version

--- draftAndDeployRelease.ps1
+++ draftAndDeployRelease.ps1
@@ -18,22 +18,22 @@ try {
       throw "No version found in the CHANGELOG.md file."
   }
 
-  $splatParams = @{
-      Uri         = "https://api.github.com/repos/$UserName/$Repository/releases"
-      Method      = 'POST'
-      Headers     = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($GHToken + ":x-oauth-basic")) }
-      ContentType = 'application/json'
-      Body        = @{
-          tag_name         = $latestVersion
-          target_commitish = 'main'
-          name             = 'Initial'
-          body             = 'Initial release'
-          draft            = $false
-          prerelease       = $false
-      } | ConvertTo-Json
-  }
-  $response = Invoke-RestMethod @splatParams
-  $message = "Created release with tag: $($response.tag_name)"
+  # $splatParams = @{
+  #     Uri         = "https://api.github.com/repos/$UserName/$Repository/releases"
+  #     Method      = 'POST'
+  #     Headers     = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($GHToken + ":x-oauth-basic")) }
+  #     ContentType = 'application/json'
+  #     Body        = @{
+  #         tag_name         = $latestVersion
+  #         target_commitish = 'main'
+  #         name             = 'Initial'
+  #         body             = 'Initial release'
+  #         draft            = $false
+  #         prerelease       = $false
+  #     } | ConvertTo-Json
+  # }
+  # $response = Invoke-RestMethod @splatParams
+  $message = "Created release with tag: $latestVersion"
 } catch {
   $PSCmdlet.ThrowTerminatingError($_)
 }
